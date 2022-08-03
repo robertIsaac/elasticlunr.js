4125ec16aedbbeb0cc4cb27882dd2475855fddd6
Robert Isaac (c) <irobert@vmware.com>
2022-08-03 15:39:33 +0300

    Build version 0.9.7

--- elasticlunr.js
+++ elasticlunr.js
@@ -1,9 +1,9 @@
 /**
  * elasticlunr - http://weixsong.github.io
- * Lightweight full-text search engine in Javascript for browser search and offline search. - 0.9.5
+ * Lightweight full-text search engine in Javascript for browser search and offline search. - 0.9.7
  *
- * Copyright (C) 2017 Oliver Nightingale
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Oliver Nightingale
+ * Copyright (C) 2022 Wei Song
  * MIT Licensed
  * @license
  */
@@ -12,8 +12,8 @@
 
 /*!
  * elasticlunr.js
- * Copyright (C) 2017 Oliver Nightingale
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Oliver Nightingale
+ * Copyright (C) 2022 Wei Song
  */
 
 /**
@@ -83,7 +83,7 @@ var elasticlunr = function (config) {
   return idx;
 };
 
-elasticlunr.version = "0.9.5";
+elasticlunr.version = "0.9.7";
 
 // only used this to make elasticlunr.js compatible with lunr-languages
 // this is a trick to define a global alias of elasticlunr
@@ -91,8 +91,8 @@ lunr = elasticlunr;
 
 /*!
  * elasticlunr.utils
- * Copyright (C) 2017 Oliver Nightingale
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Oliver Nightingale
+ * Copyright (C) 2022 Wei Song
  */
 
 /**
@@ -134,8 +134,8 @@ elasticlunr.utils.toString = function (obj) {
 };
 /*!
  * elasticlunr.EventEmitter
- * Copyright (C) 2017 Oliver Nightingale
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Oliver Nightingale
+ * Copyright (C) 2022 Wei Song
  */
 
 /**
@@ -222,8 +222,8 @@ elasticlunr.EventEmitter.prototype.hasHandler = function (name) {
 };
 /*!
  * elasticlunr.tokenizer
- * Copyright (C) 2017 Oliver Nightingale
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Oliver Nightingale
+ * Copyright (C) 2022 Wei Song
  */
 
 /**
@@ -308,8 +308,8 @@ elasticlunr.tokenizer.getSeperator = function() {
 }
 /*!
  * elasticlunr.Pipeline
- * Copyright (C) 2017 Oliver Nightingale
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Oliver Nightingale
+ * Copyright (C) 2022 Wei Song
  */
 
 /**
@@ -563,8 +563,8 @@ elasticlunr.Pipeline.prototype.toJSON = function () {
 };
 /*!
  * elasticlunr.Index
- * Copyright (C) 2017 Oliver Nightingale
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Oliver Nightingale
+ * Copyright (C) 2022 Wei Song
  */
 
 /**
@@ -885,11 +885,6 @@ elasticlunr.Index.prototype.getFields = function () {
  */
 elasticlunr.Index.prototype.search = function (query, userConfig) {
   if (!query) return [];
-  if (typeof query === 'string') {
-    query = {any: query};
-  } else {
-    query = JSON.parse(JSON.stringify(query));
-  }
 
   var configStr = null;
   if (userConfig != null) {
@@ -898,24 +893,12 @@ elasticlunr.Index.prototype.search = function (query, userConfig) {
 
   var config = new elasticlunr.Configuration(configStr, this.getFields()).get();
 
-  var queryTokens = {};
-  var queryFields = Object.keys(query);
-
-  for (var i = 0; i < queryFields.length; i++) {
-    var key = queryFields[i];
-
-    queryTokens[key] = this.pipeline.run(elasticlunr.tokenizer(query[key]));
-  }
+  var queryTokens = this.pipeline.run(elasticlunr.tokenizer(query));
 
   var queryResults = {};
 
   for (var field in config) {
-    var tokens = queryTokens[field] || queryTokens.any;
-    if (!tokens) {
-      continue;
-    }
-
-    var fieldSearchResults = this.fieldSearch(tokens, field, config);
+    var fieldSearchResults = this.fieldSearch(queryTokens, field, config);
     var fieldBoost = config[field].boost;
 
     for (var docRef in fieldSearchResults) {
@@ -932,13 +915,8 @@ elasticlunr.Index.prototype.search = function (query, userConfig) {
   }
 
   var results = [];
-  var result;
   for (var docRef in queryResults) {
-    result = {ref: docRef, score: queryResults[docRef]};
-    if (this.documentStore.hasDoc(docRef)) {
-      result.doc = this.documentStore.getDoc(docRef);
-    }
-    results.push(result);
+    results.push({ref: docRef, score: queryResults[docRef]});
   }
 
   results.sort(function (a, b) { return b.score - a.score; });
@@ -971,28 +949,28 @@ elasticlunr.Index.prototype.fieldSearch = function (queryTokens, fieldName, conf
       tokens = this.index[fieldName].expandToken(token);
     }
     // Consider every query token in turn. If expanded, each query token
-    // corresponds to a set of tokens, which is all tokens in the
+    // corresponds to a set of tokens, which is all tokens in the 
     // index matching the pattern queryToken* .
     // For the set of tokens corresponding to a query token, find and score
-    // all matching documents. Store those scores in queryTokenScores,
+    // all matching documents. Store those scores in queryTokenScores, 
     // keyed by docRef.
     // Then, depending on the value of booleanType, combine the scores
     // for this query token with previous scores.  If booleanType is OR,
     // then merge the scores by summing into the accumulated total, adding
-    // new document scores are required (effectively a union operator).
-    // If booleanType is AND, accumulate scores only if the document
+    // new document scores are required (effectively a union operator). 
+    // If booleanType is AND, accumulate scores only if the document 
     // has previously been scored by another query token (an intersection
-    // operation0.
-    // Furthermore, since when booleanType is AND, additional
+    // operation0. 
+    // Furthermore, since when booleanType is AND, additional 
     // query tokens can't add new documents to the result set, use the
-    // current document set to limit the processing of each new query
+    // current document set to limit the processing of each new query 
     // token for efficiency (i.e., incremental intersection).
-
+    
     var queryTokenScores = {};
     tokens.forEach(function (key) {
       var docs = this.index[fieldName].getDocs(key);
       var idf = this.idf(key, fieldName);
-
+      
       if (scores && booleanType == 'AND') {
           // special case, we can rule out documents that have been
           // already been filtered out because they weren't scored
@@ -1039,7 +1017,7 @@ elasticlunr.Index.prototype.fieldSearch = function (queryTokens, fieldName, conf
         }
       }
     }, this);
-
+    
     scores = this.mergeScores(scores, queryTokenScores, booleanType);
   }, this);
 
@@ -1061,7 +1039,7 @@ elasticlunr.Index.prototype.fieldSearch = function (queryTokens, fieldName, conf
 
 elasticlunr.Index.prototype.mergeScores = function (accumScores, scores, op) {
     if (!accumScores) {
-        return scores;
+        return scores; 
     }
     if (op == 'AND') {
         var intersection = {};
@@ -1180,7 +1158,7 @@ elasticlunr.Index.prototype.use = function (plugin) {
 };
 /*!
  * elasticlunr.DocumentStore
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Wei Song
  */
 
 /**
@@ -1374,8 +1352,8 @@ function clone(obj) {
 }
 /*!
  * elasticlunr.stemmer
- * Copyright (C) 2017 Oliver Nightingale
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Oliver Nightingale
+ * Copyright (C) 2022 Wei Song
  * Includes code from - http://tartarus.org/~martin/PorterStemmer/js.txt
  */
 
@@ -1593,9 +1571,10 @@ elasticlunr.stemmer = (function(){
 elasticlunr.Pipeline.registerFunction(elasticlunr.stemmer, 'stemmer');
 /*!
  * elasticlunr.stopWordFilter
- * Copyright (C) 2017 Oliver Nightingale
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Oliver Nightingale
+ * Copyright (C) 2022 Wei Song
  */
+
 
 /**
  * elasticlunr.stopWordFilter is an English language stop words filter, any words
@@ -1610,6 +1589,18 @@ elasticlunr.Pipeline.registerFunction(elasticlunr.stemmer, 'stemmer');
  * @return {String}
  * @see elasticlunr.Pipeline
  */
+elasticlunr.generateStopWordFilter = function (stopWords) {
+  var words = stopWords.reduce(function (memo, stopWord) {
+    memo[stopWord] = stopWord;
+    return memo;
+  }, {});
+
+  return function (token) {
+    if (token && words[token.toString()] !== token.toString()) return token;
+    return null;
+  };
+};
+
 elasticlunr.stopWordFilter = function (token) {
   if (token && elasticlunr.stopWordFilter.stopWords[token] !== true) {
     return token;
@@ -1630,7 +1621,7 @@ elasticlunr.clearStopWords = function () {
 /**
  * Add customized stop words
  * user could use this function to add customized stop words
- * 
+ *
  * @params {Array} words customized stop words
  * @return {null}
  */
@@ -1780,8 +1771,8 @@ elasticlunr.stopWordFilter.stopWords = elasticlunr.defaultStopWords;
 elasticlunr.Pipeline.registerFunction(elasticlunr.stopWordFilter, 'stopWordFilter');
 /*!
  * elasticlunr.trimmer
- * Copyright (C) 2017 Oliver Nightingale
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Oliver Nightingale
+ * Copyright (C) 2022 Wei Song
  */
 
 /**
@@ -1811,7 +1802,7 @@ elasticlunr.trimmer = function (token) {
 elasticlunr.Pipeline.registerFunction(elasticlunr.trimmer, 'trimmer');
 /*!
  * elasticlunr.InvertedIndex
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Wei Song
  * Includes code from - http://tartarus.org/~martin/PorterStemmer/js.txt
  */
 
@@ -2046,7 +2037,7 @@ elasticlunr.InvertedIndex.prototype.toJSON = function () {
 
 /*!
  * elasticlunr.Configuration
- * Copyright (C) 2017 Wei Song
+ * Copyright (C) 2022 Wei Song
  */
  
  /** 
@@ -2237,7 +2228,7 @@ elasticlunr.Configuration.prototype.reset = function () {
 
 /*!
  * lunr.SortedSet
- * Copyright (C) 2017 Oliver Nightingale
+ * Copyright (C) 2022 Oliver Nightingale
  */
 
 /**
