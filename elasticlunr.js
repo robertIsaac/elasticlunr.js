58750504931b21ae7f4d470a269a930abcb84c1f
Robert Isaac (c) <irobert@vmware.com>
2022-08-03 15:42:26 +0300

    Build version 0.9.6

--- elasticlunr.js
+++ elasticlunr.js
@@ -1,6 +1,6 @@
 /**
  * elasticlunr - http://weixsong.github.io
- * Lightweight full-text search engine in Javascript for browser search and offline search. - 0.9.7
+ * Lightweight full-text search engine in Javascript for browser search and offline search. - 0.9.6
  *
  * Copyright (C) 2022 Oliver Nightingale
  * Copyright (C) 2022 Wei Song
@@ -83,7 +83,7 @@ var elasticlunr = function (config) {
   return idx;
 };
 
-elasticlunr.version = "0.9.7";
+elasticlunr.version = "0.9.6";
 
 // only used this to make elasticlunr.js compatible with lunr-languages
 // this is a trick to define a global alias of elasticlunr
